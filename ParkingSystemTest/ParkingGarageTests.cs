ff6467cf5d77b3e8a3c32c7930375ef3980ccb43
Zara0798 <zara.ahmadi98@gmail.com>
2024-11-29 07:22:50 +0100

    unit tests

---
+++ ParkingSystemTest/ParkingGarageTests.cs
@@ -0,0 +1,49 @@
+﻿using ParkingSystem.Classes_Folder;
+using ParkingSystem.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingSystemTest
+{
+    [TestClass]
+
+    public class ParkingGarageTests
+    {
+        [TestMethod]
+        public void ClearParkingGarage_ShouldClearAllParkedVehicles()
+        {
+            // Arrange
+            var config = new Config
+            {
+                ParkingConfig = new ParkingConfig { TotalSpots = 5 },
+                PriceList = new List<PriceList>()
+            };
+
+            var parkedDataConfig = new ParkedDataConfig
+            {
+                ParkedVehicles = new Dictionary<string, ParkedVehicle>
+                {
+                    { "ABC123", new ParkedVehicle { Spot = 1, StartTime = DateTime.Now.AddHours(-1), Type = VehicleType.Car } },
+                    { "XYZ789", new ParkedVehicle { Spot = 2, StartTime = DateTime.Now.AddHours(-2), Type = VehicleType.Motorcycle } }
+                }
+            };
+
+            var parkingGarage = new ParkingGarage(config, parkedDataConfig);
+
+            // Act
+            parkingGarage.ClearParkingGarage();
+
+            // Assert
+            foreach (var spot in parkingGarage.Garage)
+            {
+                Assert.AreEqual(0, spot.ParkedVehicles.Count);
+                Assert.AreEqual(0, spot.OccupiedSize);
+                Assert.IsFalse(spot.IsOccupied);
+            }
+        }
+
+    }
+}
---
+++ ParkingSystemTest/ParkingSpotTests.cs
@@ -0,0 +1,32 @@
+﻿using ParkingSystem.Classes_Folder;
+using ParkingSystem.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingSystemTest
+{
+        [TestClass]
+        public class ParkingSpotTests
+        {
+            [TestMethod]
+            public void ParkingSpot_Constructor_ShouldInitializeProperties()
+            {
+                // Arrange
+                int spotNumber = 1;
+
+                // Act
+                var parkingSpot = new ParkingSpot(spotNumber);
+
+                // Assert
+                Assert.AreEqual(spotNumber, parkingSpot.SpotNumber);
+                Assert.AreEqual(1.0, parkingSpot.Capacity); // Default capacity for one car spot
+                Assert.AreEqual(0, parkingSpot.OccupiedSize);
+                Assert.IsNull(parkingSpot.ParkedTime);
+                Assert.IsNotNull(parkingSpot.ParkedVehicles);
+                Assert.AreEqual(0, parkingSpot.ParkedVehicles.Count);
+            }
+        }
+}
